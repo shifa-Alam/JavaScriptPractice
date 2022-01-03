2dd99170cf9957aec0c2239f128f1c76121ac387
shifa-Alam <shifa7446@gmail.com>
2022-01-03 20:38:51 +0600

    updated

--- Event Listeners with multiple elements/index.js
+++ Event Listeners with multiple elements/index.js
@@ -1,1 +1,11 @@
 
+var buttonLength = document.querySelectorAll(".myButton").length;
+for(var i=0;i<buttonLength;i++){
+    document.querySelectorAll(".myButton")[i].addEventListener("click", function () {
+        var text = this.innerHTML;
+    
+        document.querySelector("h1").innerHTML = text +" " + "is clicked";
+    
+    });
+    
+}
