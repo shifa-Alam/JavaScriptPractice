5ec2c696ff2eb150f9b9f553e31a3d3db591f697
shifa-Alam <shifa7446@gmail.com>
2022-01-03 21:48:43 +0600

    Audio paly

---
+++ Play Audio/index.js
@@ -0,0 +1,31 @@
+var length = document.querySelectorAll(".myButton").length;
+for (let index = 0; index < length; index++) {
+
+    document.querySelectorAll(".myButton")[index].addEventListener("click", function () {
+        var text = this.innerHTML;
+        console.log(text);
+       audioPlay(text);
+
+    });
+
+}
+
+function audioPlay(text){
+    switch (text.trim()) {
+        case "A":
+            var audio = new Audio('sounds/apple.mp3');
+            audio.play();
+            break;
+        case 'B':
+            var audio = new Audio('sounds/ball.mp3');
+            audio.play();
+            break;
+        case 'C':
+            var audio = new Audio('sounds/cat.mp3');
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+}
